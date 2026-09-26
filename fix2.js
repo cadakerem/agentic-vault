68f970d1f67bb26bd280832d83df14f4e8d82d16
CadaKerem <136694434+CadaKerem@users.noreply.github.com>
2026-09-26 22:30:16 +0300

    chore: remove helper scripts

--- fix2.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/settings/AgenticVaultSettingTab.ts', 'utf8');
-content = content.replace(/\}\);\r?\n\t\t\t\t\t\}\);\r?\n\t\t\t\}\r?\n\t\t\}\);/, '});');
-fs.writeFileSync('src/settings/AgenticVaultSettingTab.ts', content, 'utf8');
